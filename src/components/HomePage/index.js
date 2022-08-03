d3787b9778cf9732a424e08db34e343d6f01fe26
Hendrick Manullang <h.manullang@gmail.com>
2022-08-03 16:09:12 +1000

    added some buttons

--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -1,12 +1,23 @@
 import { TextField } from "@mui/material"
+import Stack from "@mui/material/Stack"
+import Button from "@mui/material/Button"
 
 const HomePage = () => {
   return (
-  <>
+  <Stack
+    direction="row"
+    justifyContent="center"
+    alignItems="center"
+    spacing={2}
+    >
   <h1>Look up a stock:</h1>
   <TextField id="standard-basic" label="Ticker" variant="standard" />
-  <button>Submit</button>
-  </>
+  <Button
+    type="submit"
+    variant="contained"
+    disabled={true}
+    >Search</Button>
+  </Stack>
   )
 }
 
