76debdcc385231ca08e75bcc0db14b1a0eb89100
Hendrick Manullang <h.manullang@gmail.com>
2022-08-03 17:35:51 +1000

    added api endpoint

--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -1,8 +1,27 @@
 import { TextField } from "@mui/material"
+import { useEffect, useState } from "react"
 import Stack from "@mui/material/Stack"
 import Button from "@mui/material/Button"
+const API_KEY = process.env.REACT_APP_API_KEY
 
 const HomePage = () => {
+  const url= `https://financialmodelingprep.com/api/v3/search-name?query=${query_string}&limit=10&exchange=NASDAQ&apikey=${API_KEY}`
+  const [stockList, setStockList] = useState([])
+
+  useEffect(() => {
+  }, [])
+
+  const loadStocks = () => {
+    fetch(url)
+      .then(response => {
+        return response.json()
+      })
+      .then(data => {
+        setStockList(data)
+        }
+      )
+  }
+
   return (
   <Stack
     direction="row"
@@ -10,7 +29,7 @@ const HomePage = () => {
     alignItems="center"
     spacing={2}
     >
-  <h1>Look up a stock:</h1>
+  <h1>Look up a company:</h1>
   <TextField id="standard-basic" label="Ticker" variant="standard" />
   <Button
     type="submit"
