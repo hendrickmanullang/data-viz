ada60b9be99f7435782897ecbd84328ec0ff773d
Hendrick Manullang <h.manullang@gmail.com>
2022-08-04 15:20:20 +1000

    linkedup stock detail to different endpoint

--- src/components/StockDetail/index.js
+++ src/components/StockDetail/index.js
@@ -1,9 +1,22 @@
 import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+const API_KEY = process.env.REACT_APP_API_KEY
 
 const StockDetail = () => {
 const { ticker } = useParams()
+const [ stock , setStock] = useState([])
 
-return <p>{ticker}</p>
+useEffect(() => {
+  const url= `https://financialmodelingprep.com/api/v3/quote/${ticker}?apikey=${API_KEY}`
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => setStock(data))
+    .catch((error) => console.error(error))
+}, [ticker])
+
+console.log(stock.price)
+
+return (<p>{stock[0].name} | stock price: {stock[0].price}</p>)
 
 }
 
