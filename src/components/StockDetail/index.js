3db896d0a57097b637169fd53a43ed6e0f3c03f5
Hendrick Manullang <h.manullang@gmail.com>
2022-08-09 17:27:39 +1000

    added date function

--- src/components/StockDetail/index.js
+++ src/components/StockDetail/index.js
@@ -7,17 +7,22 @@ import LineChart from "../LineChart"
 const API_KEY = process.env.REACT_APP_API_KEY
 
 const StockDetail = () => {
+  var currentDate = new Date()
+  var currentDateISO = currentDate.toISOString().slice(0,10)
+  var defaultSearchDate = new Date(currentDate.getFullYear() - 1, currentDate.getMonth(), currentDate.getDate())
+  var defaultSearchDateISO = defaultSearchDate.toISOString().slice(0,10)
+
   const { ticker } = useParams()
-  const [stockPrice , setStockPrice] = useState(null)
+  const [stockPrice, setStockPrice] = useState(null)
   const [stockNews, setStockNews] = useState(null)
-  const [searchDate, setSearchDate] = useState({})
   const [stockHistoricalPrice, setStockHistoricalPrice] = useState([])
+  // const [searchHistoricalStartDate, setHistoricalSearchStartDate] = useState(currentDate) -> add a method here to
   const [date, setDate] = useState([])
 
   useEffect(() => {
     const priceEndPoint= `https://financialmodelingprep.com/api/v3/quote/${ticker}?apikey=${API_KEY}`
     const newsEndPoint= `https://financialmodelingprep.com/api/v3/stock_news?tickers=${ticker}&limit=3&apikey=${API_KEY}`
-    const historicalPriceEndPoint= `https://financialmodelingprep.com/api/v3/historical-price-full/${ticker}?from=2021-08-09&to=2022-08-09&apikey=${API_KEY}`
+    const historicalPriceEndPoint= `https://financialmodelingprep.com/api/v3/historical-price-full/${ticker}?from=${defaultSearchDateISO}&to=${currentDateISO}&apikey=${API_KEY}`
 
     fetch(priceEndPoint)
       .then((priceResponse) => priceResponse.json())
